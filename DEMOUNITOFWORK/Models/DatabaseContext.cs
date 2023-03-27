642efc20aee71d35ef5b14b3bb4d30eca2cc6294
xuyen le <letuanxuyen92@gmail.com>
2023-03-27 18:47:19 +0700

    complete database context and migration

---
+++ DEMOUNITOFWORK/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DEMOUNITOFWORK.Models
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions options):base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Order>().HasKey(o => o.Id);
+        }
+        public DbSet<Product>Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
--- DEMOUNITOFWORK/Program.cs
+++ DEMOUNITOFWORK/Program.cs
@@ -1,8 +1,11 @@
+using DEMOUNITOFWORK.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<DatabaseContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectDB")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
