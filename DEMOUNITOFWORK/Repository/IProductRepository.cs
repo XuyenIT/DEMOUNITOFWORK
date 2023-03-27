a4147b71a885fcdc4babd2d99e5fbb7cc4da08da
xuyen le <letuanxuyen92@gmail.com>
2023-03-27 19:08:33 +0700

    complete interface repository

---
+++ DEMOUNITOFWORK/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using DEMOUNITOFWORK.Models;
+
+namespace DEMOUNITOFWORK.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetProducts();
+        Product GetProduct (int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+}
